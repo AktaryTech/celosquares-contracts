a71e788fc145bf6c0d71dbf9d316b1d584dc0198
Dan Hepworth <djh58@duke.edu>
2021-05-15 15:25:44 -0400

    added contracts folder and my start to the game logic

---
+++ contracts/gameLogic.sol
@@ -0,0 +1,96 @@
+pragma solidity ^0.7.6;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+contract Game is Context, Ownable {
+    address private _owner;
+
+    string public teamOne;
+    string public teamTwo;
+    uint256 betAmount;
+
+    struct Square {
+        address bettor;
+        bool hasBet; 
+        uint256 firstTeamDigit;
+        uint256 secondTeamDigit; 
+    }
+
+    Square[10][10] bets; 
+
+    constructor(string memory firstTeam, string memory secondTeam, uint256 bet) {
+        _owner = _msgSender();
+        teamOne = firstTeam;
+        teamTwo = secondTeam;
+        betAmount = bet; 
+    }
+
+    function placeBet(uint256 x, uint256 y) public {
+        require(Square[x][y].hasBet == false, "This square already has a bet placed");
+        Square storage mySquare = Square[x][y];
+        mySquare.bettor = _msgSender;
+        mySquare.hasBet = true;
+    }
+
+}
