a51382cb86664fb1e5be348d3810f96f2e3f8f7c
JaredChilders <jared.childers97@gmail.com>
2021-05-15 17:31:57 -0400

    changes

--- contracts/Scoracle.sol
+++ contracts/Scoracle.sol
@@ -13,7 +13,9 @@ contract Scoracle is Ownable {
         uint256 quarter;
     }
 
-    mapping(uint256 => mapping(uint256 => Team)) public teamStatsForQuarter;
+    //mapping(uint256 => mapping(uint256 => Team)) public teamStatsForQuarter;
+
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => Team))) public teamStatsForQuarter;
 
     event ScorePublished(
         uint256 indexed gameid, 
@@ -30,49 +32,60 @@ contract Scoracle is Ownable {
     /**
      * @dev - Call this function to send the data in the smart contract
      */
-    function response(uint256 _gameid, uint256 _quarter, uint256 _team1id, uint256 _team2id, uint256 _team1score, uint256 _team2score) external onlyOwner {
+    function recordScore(uint256 _gameid, uint256 _quarter, uint256 _team1id, uint256 _team2id, uint256 _team1score, uint256 _team2score) external onlyOwner {
 
         // set team 1 stats for Quarter
-        teamStatsForQuarter[_quarter][_team1id].gameID = _gameid;
-        teamStatsForQuarter[_quarter][_team1id].quarter = _quarter;
-        teamStatsForQuarter[_quarter][_team1id].teamID = _team1id;
-        teamStatsForQuarter[_quarter][_team1id].score = _team1score;
+        teamStatsForQuarter[_gameid][_quarter][_team1id].gameID = _gameid;
+        teamStatsForQuarter[_gameid][_quarter][_team1id].quarter = _quarter;
+        teamStatsForQuarter[_gameid][_quarter][_team1id].teamID = _team1id;
+        teamStatsForQuarter[_gameid][_quarter][_team1id].score = _team1score;
 
         // set team 2 stats for Quarter
-        teamStatsForQuarter[_quarter][_team2id].gameID = _gameid;
-        teamStatsForQuarter[_quarter][_team2id].quarter = _quarter;
-        teamStatsForQuarter[_quarter][_team2id].teamID = _team2id;
-        teamStatsForQuarter[_quarter][_team2id].score = _team2score;
+        teamStatsForQuarter[_gameid][_quarter][_team2id].gameID = _gameid;
+        teamStatsForQuarter[_gameid][_quarter][_team2id].quarter = _quarter;
+        teamStatsForQuarter[_gameid][_quarter][_team2id].teamID = _team2id;
+        teamStatsForQuarter[_gameid][_quarter][_team2id].score = _team2score;
 
         // emit ScorePublished event to log the timestamp at which the data enters the contract
         emit ScorePublished(
             _gameid, 
             _quarter, 
-            teamStatsForQuarter[_quarter][_team1id].quarter, 
-            teamStatsForQuarter[_quarter][_team2id].teamID, 
-            teamStatsForQuarter[_quarter][_team1id].score, 
-            teamStatsForQuarter[_quarter][_team2id].score, 
+            teamStatsForQuarter[_gameid][_quarter][_team1id].quarter, 
+            teamStatsForQuarter[_gameid][_quarter][_team2id].teamID, 
+            teamStatsForQuarter[_gameid][_quarter][_team1id].score, 
+            teamStatsForQuarter[_gameid][_quarter][_team2id].score, 
             block.timestamp
         );
     }
 
-    function getTeamDataForQuarter(uint256 _quarter, uint256 _teamid) internal view returns(uint256, uint256, uint256) {
+    function getTeamDataForQuarter(uint256 _gameid, uint256 _quarter, uint256 _teamid) internal view returns(uint256, uint256, uint256) {
         return (
-            teamStatsForQuarter[_quarter][ _teamid].gameID, 
-            teamStatsForQuarter[_quarter][ _teamid].teamID, 
-            teamStatsForQuarter[_quarter][ _teamid].score
+            teamStatsForQuarter[_gameid][_quarter][_teamid].gameID, 
+            teamStatsForQuarter[_gameid][_quarter][_teamid].teamID, 
+            teamStatsForQuarter[_gameid][_quarter][_teamid].score
         );
     }
 
-    function getTeamsDataForQuarter(uint256 _quarter, uint256 _team1id, uint256 _team2id) internal view returns(uint256, uint256, uint256, uint256, uint256, uint256) {
+    function getTeamsDataForQuarter(uint256 _gameid, uint256 _quarter, uint256 _team1id, uint256 _team2id) internal view returns(uint256, uint256, uint256, uint256, uint256, uint256) {
         return (
-            teamStatsForQuarter[_quarter][ _team1id].gameID, 
-            teamStatsForQuarter[_quarter][ _team1id].teamID, 
-            teamStatsForQuarter[_quarter][ _team1id].score,
-            teamStatsForQuarter[_quarter][ _team2id].gameID, 
-            teamStatsForQuarter[_quarter][ _team2id].teamID, 
-            teamStatsForQuarter[_quarter][ _team2id].score
+            teamStatsForQuarter[_gameid][_quarter][_team1id].gameID, 
+            teamStatsForQuarter[_gameid][_quarter][_team1id].teamID, 
+            teamStatsForQuarter[_gameid][_quarter][_team1id].score,
+            teamStatsForQuarter[_gameid][_quarter][_team2id].gameID, 
+            teamStatsForQuarter[_gameid][_quarter][_team2id].teamID, 
+            teamStatsForQuarter[_gameid][_quarter][_team2id].score
         );
+    }
+    
+    function clearGameData(uint256 _gameid, uint256 _team1id, uint256 _team2id) internal onlyOwner {
+        for(uint i = 0; i < 4; i++) {
+            delete teamStatsForQuarter[_gameid][i][_team1id].gameID; 
+            delete teamStatsForQuarter[_gameid][i][_team1id].teamID; 
+            delete teamStatsForQuarter[_gameid][i][_team1id].score;
+            delete teamStatsForQuarter[_gameid][i][_team2id].gameID; 
+            delete teamStatsForQuarter[_gameid][i][_team2id].teamID; 
+            delete teamStatsForQuarter[_gameid][i][_team2id].score;
+        }
     }
 
 }
